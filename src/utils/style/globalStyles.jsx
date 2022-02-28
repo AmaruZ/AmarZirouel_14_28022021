2e80c2c807f64b140f0b1fd4718a3f73e9b8e0fa
Amar <a.zirouel@gmail.com>
2022-02-28 15:16:49 +0100

    First commit

---
+++ src/utils/style/globalStyles.jsx
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+    html {
+        font-family: Montserrat, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        box-sizing: border-box;
+    }
+    *{
+        padding: 0;
+        margin: 0;
+    }
+`
+
+export default GlobalStyle
