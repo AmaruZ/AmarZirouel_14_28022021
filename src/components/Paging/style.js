b035b9ee8cafe21902f55f3d8364432ea8361007
Amar <a.zirouel@gmail.com>
2022-04-26 04:13:25 +0200

    Version de presentation

--- src/components/Paging/style.js
+++ src/components/Paging/style.js
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 export const PagingContainer = styled.div`
     display: flex;
     justify-content: space-between;
+    margin-bottom: 20px;
 `
 export const PagingNumbersButton = styled.button`
     width: 23px;
